099fdfca64480abde06d7371e1598dfae6a3310b
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-07-15 19:27:32 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd src
 make
+cp system.make ../tools/BasisGeneration
 cd ../tools/BasisGeneration
 make
+cp system.make ../PostProcessing
 cd ../PostProcessing
 make
 cp ../bin/Conquest ${PREFIX}/bin/Conquest
